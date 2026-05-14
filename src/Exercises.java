b5297ea18b34c75e3f7ba30687de926e6fc64307
Un01 <274392693+alexanderpena014-svg@users.noreply.github.com>
2026-05-13 17:09:53 -0700

    my partner and I completed the first 3 exercies but I got rid of them to show that Ive commiedt/push every time. so youll see them completed seconds from each other

--- src/Exercises.java
+++ src/Exercises.java
@@ -12,7 +12,15 @@ public class Exercises {
      * @return the length of the list
      */
     public static int length(ListNode head) {
-        return -1;
+        int count = 0;
+
+        ListNode current = head;
+
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
     }
 
     /**
@@ -29,7 +37,7 @@ public class Exercises {
      * @return the new head of the linked list
      */
     public static ListNode prepend(ListNode head, int toAdd) {
-        return null;
+        
     }
 
     /**
@@ -45,7 +53,7 @@ public class Exercises {
      * @return the head of the list with the last element removed
      */
     public static ListNode removeLast(ListNode head) {
-        return null;
+        
     }
 
     /**
